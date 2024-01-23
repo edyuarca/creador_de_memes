04ed83b4e25246a718a51bcf6b910f0157eda573
Yuli <edyuarca_15@hotmail.com>
2024-01-22 23:05:37 -0300

    por problemas con git no se pudo subir el repositorio sino hasta cuando se finalizó el armado y estil ado de la página creadora de memes

---
+++ main.js
@@ -0,0 +1,13 @@
+/*barra navegacion*/
+
+const iconoImagen = document.getElementById("icono_imagen")
+const iconoTexto = document.getElementById("icono_texto")
+const modoOscuro = document.getElementById("modo_oscuro")
+
+/*contenedor meme*/
+
+/*aside imagen*/
+
+const campoUrl = document.getElementById("url")
+const fondo = document.getElementById("fondo")
+const fondo_desplegable = document.getElementById("fondo_desplegable")
