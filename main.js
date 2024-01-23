19a2dc0a1aec8d30ba5c2ddbb148d97120c751a3
Yuli <edyuarca_15@hotmail.com>
2024-01-23 00:24:22 -0300

    se generan todas las variables que llaman a los elementos -  DOM

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ const modoOscuro = document.getElementById("modo_oscuro")
 const campoUrl = document.getElementById("url")
 const fondo = document.getElementById("fondo")
 const fondo_desplegable = document.getElementById("fondo_desplegable")
+
+const brillo = document.getElementById("brillo")
+const opacidad = document.getElementById("opacidad")
+const contraste = document.getElementById("contraste")
+const desenfoque = document.getElementById("desenfoque")
+const escalaDeGrises = document.getElementById("escala_de_grises")
+const sepia = document.getElementById("sepia")
+const hue = document.getElementById("hue")
+const saturado = document.getElementById("saturado")
+const negativo = document.getElementById("negativo")
+
+const reestablecerFiltros = document.getElementById("reestablecer_filtros")
+
