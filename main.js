bfc056e257eb4d7755fe993d42e922032677d2bb
Yuli <edyuarca_15@hotmail.com>
2024-02-01 10:29:47 -0300

    se modificó background image y fondo color, queda pendiente verificar modoFusion

--- main.js
+++ main.js
@@ -1,149 +1,152 @@
-const body = document.getElementById("body")
+const body = document.getElementById("body");
 
 /*barra navegacion*/
 
-const iconoImagen = document.getElementById("icono_imagen")
-const iconoTexto = document.getElementById("icono_texto")
-const modoOscuro = document.getElementById("modo_oscuro")
-const modoClaro = document.getElementById( "modo_claro")
+const iconoImagen = document.getElementById("icono_imagen");
+const iconoTexto = document.getElementById("icono_texto");
+const modoOscuro = document.getElementById("modo_oscuro");
+const modoClaro = document.getElementById("modo_claro");
 
 /*contenedor meme*/
 
-const memeBox = document.getElementById("meme_box")
-const boxTxtSuperior = document.getElementById("txt_superior")
-const boxImagen = document.getElementById("box_img_meme")
-const boxTxtInferior = document.getElementById("txt_inferior")
-const descargarMeme = document.getElementById("descargar_meme")
+const memeBox = document.getElementById("meme_box");
+const imagenMeme = document.querySelector(".imagen_meme");
+const boxTxtSuperior = document.getElementById("txt_superior");
+const boxImagen = document.getElementById("box_img_meme");
+const boxTxtInferior = document.getElementById("txt_inferior");
+const descargarMeme = document.getElementById("descargar_meme");
 
 /*aside imagen*/
 
-const asideImagen = document.getElementById("aside_imagen")
-const campoUrl = document.getElementById("url")
-const fondo = document.getElementById("fondo")
-const fondo_desplegable = document.getElementById("fondo_desplegable")
+const asideImagen = document.getElementById("aside_imagen");
+const campoUrl = document.getElementById("url");
+const fondo = document.getElementById("fondo");
+const fondoDesplegable = document.getElementById("fondo_desplegable");
+const colorHex = document.getElementById("color_hex");
 
-const brillo = document.getElementById("brillo")
-const opacidad = document.getElementById("opacidad")
-const contraste = document.getElementById("contraste")
-const desenfoque = document.getElementById("desenfoque")
-const escalaDeGrises = document.getElementById("escala_de_grises")
-const sepia = document.getElementById("sepia")
-const hue = document.getElementById("hue")
-const saturado = document.getElementById("saturado")
-const negativo = document.getElementById("negativo")
+const brillo = document.getElementById("brillo");
+const opacidad = document.getElementById("opacidad");
+const contraste = document.getElementById("contraste");
+const desenfoque = document.getElementById("desenfoque");
+const escalaDeGrises = document.getElementById("escala_de_grises");
+const sepia = document.getElementById("sepia");
+const hue = document.getElementById("hue");
+const saturado = document.getElementById("saturado");
+const negativo = document.getElementById("negativo");
 
-const reestablecerFiltros = document.getElementById("reestablecer_filtros")
+const reestablecerFiltros = document.getElementById("reestablecer_filtros");
 
 /*aside texto*/
 
-const asideTexto = document.getElementById("aside_texto")
-const textoSuperior = document.getElementById("superior")
-const sinTextoSup = document.getElementById("sin_texto_sup")
-const textoInferior = document.getAnimations("inferior")
-const sinTextoInf = document.getElementById("sin_texto_inf")
+const asideTexto = document.getElementById("aside_texto");
+const textoSuperior = document.getElementById("superior");
+const sinTextoSup = document.getElementById("sin_texto_sup");
+const textoInferior = document.getAnimations("inferior");
+const sinTextoInf = document.getElementById("sin_texto_inf");
 
-const fuentesDesplegables = document.getElementById("fuentes_desplegables")
-const tamanoFuente = document.getElementById("tamano_fuente")
-const textoIzquierda = document.getElementById("izquierda")
-const textoCentro = document.getElementById("centro")
-const textoDerecha = document.getElementById("derecha")
+const fuentesDesplegables = document.getElementById("fuentes_desplegables");
+const tamanoFuente = document.getElementById("tamano_fuente");
+const textoIzquierda = document.getElementById("izquierda");
+const textoCentro = document.getElementById("centro");
+const textoDerecha = document.getElementById("derecha");
 
-const colorTexto = document.getElementById("color")
-const fondoTexto = document.getElementById("fondo")
-const fondoTransparente = document.getElementById("fondo_transparente")
+const colorTexto = document.getElementById("color");
+const fondoTexto = document.getElementById("fondo");
+const fondoTransparente = document.getElementById("fondo_transparente");
 
-const noContorno = document.getElementById("ningun_contorno")
-const contornoClaro = document.getElementById("claro")
-const contornoOscuro = document.getElementById("oscuro")
+const noContorno = document.getElementById("ningun_contorno");
+const contornoClaro = document.getElementById("claro");
+const contornoOscuro = document.getElementById("oscuro");
 
-const espaciado = document.getElementById("espaciado")
-const interlineado = document.getElementById("interlineado")
+const espaciado = document.getElementById("espaciado");
+const interlineado = document.getElementById("interlineado");
 
 /*Funciones paneles*/
 
 const ocultarPanelImagen = () => {
-    asideImagen.style.display = "none"
-}
+  asideImagen.style.display = "none";
+};
 const mostrarPanelImagen = () => {
-    asideImagen.style.display = "block"
-}
+  asideImagen.style.display = "block";
+};
 const ocultarPanelTexto = () => {
-    asideTexto.style.display = "none"
-}
+  asideTexto.style.display = "none";
+};
 const mostrarPanelTexto = () => {
-    asideTexto.style.display = "block"
-}
+  asideTexto.style.display = "block";
+};
 
 const activarModoOscuro = () => {
-    body.style.backgroundColor = "rgb(68, 73, 79)"
-}
+  body.style.backgroundColor = "rgb(68, 73, 79)";
+};
 const desactivarModoOscuro = () => {
-    body.style.backgroundColor = "rgb(255, 255, 255)"
-}
+  body.style.backgroundColor = "rgb(255, 255, 255)";
+};
 
 const visualizacionFondoModos1 = () => {
-    if (body.style.backgroundColor === "rgb(68, 73, 79)" ) {desactivarModoOscuro ();} /*esta funcion puede ser remplazada por la de la linea 93*/
-    else {activarModoOscuro ();}
-}
+  if (body.style.backgroundColor === "rgb(68, 73, 79)") {
+    desactivarModoOscuro();
+  } /*esta funcion puede ser remplazada por la de la linea 93*/ else {
+    activarModoOscuro();
+  }
+};
 
 const visualizacionFondoModos2 = () => {
-    if (body.style.backgroundColor === "rgb(255, 255, 255)" ) {activarModoOscuro ();} /*esta funcion puede ser remplazada por la de la linea 93*/
-    else {desactivarModoOscuro ();}
-}
+  if (body.style.backgroundColor === "rgb(255, 255, 255)") {
+    activarModoOscuro();
+  } /*esta funcion puede ser remplazada por la de la linea 93*/ else {
+    desactivarModoOscuro();
+  }
+};
 
 const activarbtnClaro = () => {
-    modoClaro.style.display = "block"
-}
+  modoClaro.style.display = "block";
+};
 const desactivarbtnoscuro = () => {
-    modoOscuro.style.display = "none"
-}
+  modoOscuro.style.display = "none";
+};
 
 const pruebaBtnModos1 = () => {
-        modoClaro.style.display = "block"
-        modoOscuro.style.display = "none"
-}
+  modoClaro.style.display = "block";
+  modoOscuro.style.display = "none";
+};
 const pruebaBtnModos2 = () => {
-    modoClaro.style.display = "none"
-    modoOscuro.style.display = "block"
-}
+  modoClaro.style.display = "none";
+  modoOscuro.style.display = "block";
+};
 
-iconoTexto.addEventListener(`click`, mostrarPanelTexto)
-iconoTexto.addEventListener(`click`, ocultarPanelImagen)
+iconoTexto.addEventListener(`click`, mostrarPanelTexto);
+iconoTexto.addEventListener(`click`, ocultarPanelImagen);
 
-iconoImagen.addEventListener(`click`, mostrarPanelImagen)
-iconoImagen.addEventListener(`click`, ocultarPanelTexto)
+iconoImagen.addEventListener(`click`, mostrarPanelImagen);
+iconoImagen.addEventListener(`click`, ocultarPanelTexto);
 
-modoOscuro.addEventListener(`click`, visualizacionFondoModos1)
-modoOscuro.addEventListener(`click`, pruebaBtnModos1)
+modoOscuro.addEventListener(`click`, visualizacionFondoModos1);
+modoOscuro.addEventListener(`click`, pruebaBtnModos1);
 
-modoClaro.addEventListener(`click`, visualizacionFondoModos2)
-modoClaro.addEventListener(`click`, pruebaBtnModos2)
+modoClaro.addEventListener(`click`, visualizacionFondoModos2);
+modoClaro.addEventListener(`click`, pruebaBtnModos2);
 
 /*boton url y box img meme */
 
-const cargarImagen = () => {
-    const imagenUrl = campoUrl.value;
-    const imagenNueva = new Image();
-    imagenNueva.src = imagenUrl 
-    imagenNueva.style.maxWidth = `100%`
-    boxImagen.innerHTML = ``;
-    boxImagen.appendChild(imagenNueva);
-}
-campoUrl.addEventListener(`input`, cargarImagen)
+const cambiarImagen = () => {
+  boxImagen.style.backgroundImage = `url("${campoUrl.value}")`;
+};
+
+campoUrl.addEventListener("input", cambiarImagen);
 
 /*boton color fondo imagen */
 
+const cambioDeColor = () => {
+  const btnColorFondo = fondo.value;
+  boxImagen.style.backgroundColor = btnColorFondo;
+  colorHex.textContent = btnColorFondo;
+};
+fondo.addEventListener(`input`, cambioDeColor);
 
+const modoFusion = (e) => {
+  boxImagen.style.backgroundBlendMode = e.target.value;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
+fondoDesplegable.addEventListener(`change`, (e) => modoFusion(e))
+console.log (modoFusion)
